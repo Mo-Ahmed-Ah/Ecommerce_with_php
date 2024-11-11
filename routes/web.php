2334b0debea2fd3b9cd6ef7d051d8cbcc1456212
Ahmed Mohamed <ahmed0033ahmed.mo@gmail.com>
2024-11-11 03:03:51 +0200

    not commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
+Route::get ('/', function ()
+    {
+
+    $result =DB::table ('categories')->get ();
+    dd($result);
     return view('welcome');
 });
